369c8a71f276ddbd8e8b60c480b3587695584447
Sahatsawat-Wattana <sahatsawat.wattanapraditchai@gmail.com>
2026-04-08 16:49:13 +0700

    debug

--- index.js
+++ index.js
@@ -96,6 +96,8 @@ class Mage extends Hero {
         `${this.name} has fallen in battle. There is no magic left to save her.`,
       );
     }
+  }
+}
 
 class Fighter extends Hero {
   constructor(name, gender, multiverse, hp, attack) {
@@ -149,6 +151,7 @@ class Assassin extends Hero {
       console.log(`${this.name} is currently hidden.`);
     }
   }
+}
 
 class Sharpshooter extends Hero {
   constructor(name, gender, multiverse, hp, attack, maxAmmo) {
